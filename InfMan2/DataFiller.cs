008b678f6fe275cd8fed951d5a26f8b585a6a2d0
JurgenHaan <0912990@hr.nl>
2018-06-19 20:24:27 +0200

    Another!
    Another!

--- InfMan2/DataFiller.cs
+++ InfMan2/DataFiller.cs
@@ -18,7 +18,7 @@ namespace InfMan2
             Data.trueFalseQuestionsList.Add(new Tuple<string, bool>("Mogen organisaties altijd mijn persoonsgegevens verwerken volgens" + System.Environment.NewLine + " de Wet bescherming persoonsgegevens (Wbp)?", false));
             Data.trueFalseQuestionsList.Add(new Tuple<string, bool>("Organisaties moeten voldoen aan een aantal eisen van de Wet bescherming persoonsgegevens"+ System.Environment.NewLine + " (Wbp) bij het verwekend van persoonsgegevens",true));
             Data.trueFalseQuestionsList.Add(new Tuple<string, bool>("Als een organisatie zich voldoet aan de eisen van de Wet " + System.Environment.NewLine + "bescherming persoonsgegevens (Wbp), betekent dit dat mijn informatie veilig is?", false));
-
+            Data.trueFalseQuestionsList.Add(new Tuple<string, bool>("Persoonsgegevens zijn geven direct of indirect informatie over een persoon", true));
 
             Data.abcdQuestionsList.Add(new ABCDModel("Wat is privacy?", "B", "Een spel", "Een afweerrecht", "Een hoax","Een ideologie"));
         }
